86ded81635072077cd43f1da8c6e731063ca47d9
Michael Sprauer <Michael@Sprauer.net>
2013-05-14 15:58:26 +0200

    Aktueller Stand (bis ca. Kapitel 3.3)

--- src/src/main/java/net/sprauer/sitzplaner/model/notifications/NotificationNameChanged.java
+++
@@ -1,9 +0,0 @@
-package net.sprauer.sitzplaner.model.notifications;
-
-public class NotificationNameChanged extends AbstractNotification {
-
-	public NotificationNameChanged(int index) {
-		super(index);
-	}
-
-}
--- src/src/main/java/net/sprauer/sitzplaner/model/notifications/NotificationStudentRemoved.java
+++
@@ -1,9 +0,0 @@
-package net.sprauer.sitzplaner.model.notifications;
-
-public class NotificationStudentRemoved extends AbstractNotification {
-
-	public NotificationStudentRemoved(int index) {
-		super(index);
-	}
-
-}
